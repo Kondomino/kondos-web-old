5f4755808bdb0dc078cbab5bfe0da7602f174c8e
Victor Souto <souto.victor@gmail.com>
2024-08-02 16:57:11 -0300

    Delete build directory

--- build/server/app/index.prefetch.rsc
+++
@@ -1,1 +0,0 @@
-0:["GQTbwvYB64jENcvM__2d_",[[["",{"children":["(home)",{"children":["__PAGE__",{}]}]},"$undefined","$undefined",true],null,null]]]
--- build/static/chunks/app/(others)/terms/page-e2d51d76b4f75f42.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6724,5902,822,7610],{63521:function(){}},function(n){n.O(0,[4121,5290,1744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
